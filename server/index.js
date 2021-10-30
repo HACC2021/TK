6bb88b7a325d1d79c82961c5077a048f120e59ed
trey-sumida <tasumida@hawaii.edu>
2021-10-29 18:57:12 -1000

    express folder structure

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import usersRoutes from './routes/users.js';
 
 const app = express();
 
@@ -11,6 +12,8 @@ app.use(cors());
 
 app.listen(3100, () => console.log("Server is running"));
 
+app.use('/users', usersRoutes);
+
 app.get( '/api',(req, res) => {
     res.send("Hello")
-})
+});
