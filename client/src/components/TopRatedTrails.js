dffa42f74baef6011862b7015d9c4fc6f91aca45
trey-sumida <tasumida@hawaii.edu>
2021-10-30 22:04:25 -1000

    added bootstrap

--- client/src/components/TopRatedTrails.js
+++ client/src/components/TopRatedTrails.js
@@ -1,10 +1,22 @@
 import TrailsInfoCard from "./TrailsInfoCard";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function TopRatedTrails() {
     return (
-        <div>
-            <h1>Top Rated Trails</h1>
-            <TrailsInfoCard />
+        <div className="top-rated-trails">
+            <h1 style={{ marginBottom: "1rem" }}>Top Rated Trails</h1>
+            <Row>
+                <Col md={true}>
+                    <TrailsInfoCard />
+                </Col>
+                <Col md={true}>
+                    <TrailsInfoCard />
+                </Col>
+                <Col md={true}>
+                    <TrailsInfoCard />
+                </Col>
+            </Row>
         </div>
     )
 }
--- client/src/components/TrailsInfoCard.js
+++ client/src/components/TrailsInfoCard.js
@@ -1,9 +1,16 @@
+import Card from 'react-bootstrap/Card';
 
 function TrailsInfoCard() {
     return (
-        <div className="trails-info-card">
-            <h1>Card</h1>
-        </div>
+        <Card border="success" className="trails-info-card">
+            <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+                </Card.Text>
+            </Card.Body>
+        </Card>
     )
 }
 
