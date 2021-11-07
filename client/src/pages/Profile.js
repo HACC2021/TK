2a9f77fc6f7760c75ff7060a7b08162d0c0e3028
trey-sumida <tasumida@hawaii.edu>
2021-11-07 09:16:05 -1000

    Update Profile.js

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,15 +1,35 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import Axios from 'axios';
 import AuthContext from '../store/auth-context';
+import Spinner from 'react-bootstrap/Spinner';
 
 function Profile() {
 
     const authCtx = useContext(AuthContext);
+    const [isLoading, setLoading] = useState(true);
 
-    const isLoggedIn = authCtx.isLoggedIn;
+    const getData = async () => {
+        const { data } = await Axios.post('http://localhost:5000/users/profile', {},
+        { headers: {
+            'authorization': `Bearer ${authCtx.token}`,
+        }});
+
+        console.log(data);
+    }
+
+    useEffect(() => {
+        getData();
+    }, )
 
     return (
         <div>
-            {isLoggedIn ? <h1>I'm logged in!</h1> : <h1>I'm not logged in.</h1>}
+            {isLoading ? 
+                <Spinner animation="border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </Spinner>
+            :
+                null
+            }
         </div>
     )
 }
