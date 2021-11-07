72c9178301e717216aa8d7592e6ddc65f91bc857
trey-sumida <tasumida@hawaii.edu>
2021-11-06 21:00:16 -1000

    basic login created

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,63 @@
+import React from 'react';
+ import { useFormik } from 'formik';
+ import Container from 'react-bootstrap/Container';
+ 
+ const validate = values => {
+   const errors = {};
 
-function Login() {
-    return (
-        <div>
-            <h1>Login Page</h1>
-        </div>
-    )
-}
+   if (!values.username) {
+       errors.username = 'Required';
+   }
 
-export default Login;
+   if (!values.password) {
+       errors.password = 'Required';
+   }
+ 
+   return errors;
+ };
+ 
+ function Login() {
+   const formik = useFormik({
+     initialValues: {
+       username: '',
+       password: '',
+     },
+     validate,
+     onSubmit: values => {
+       alert(JSON.stringify(values, null, 2));
+     },
+   });
+   return (
+       <Container>
+            <form onSubmit={formik.handleSubmit}>
+
+            <label htmlFor="username">Username</label>
+            <input
+                id="username"
+                name="username"
+                type="text"
+                onChange={formik.handleChange}
+                value={formik.values.username}
+            />
+            {formik.errors.username ? <div>{formik.errors.username}</div> : null}
+
+            <label htmlFor="password">Password</label>
+            <input
+                id="password"
+                name="password"
+                type="password"
+                onChange={formik.handleChange}
+                value={formik.values.password}
+            />
+            {formik.errors.password ? <div>{formik.errors.password}</div> : null}
+        
+            <button type="submit">Login</button>
+            </form>
+
+            <p>Don't have an account? Register <a href='/register'>here</a></p>
+     </Container>
+   );
+ };
+
+ export default Login;
+
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,8 +1,7 @@
 import React from 'react';
  import { useFormik } from 'formik';
+ import Container from 'react-bootstrap/Container';
  
- // A custom validation function. This must return an object
- // which keys are symmetrical to our values/initialValues
  const validate = values => {
    const errors = {};
 
@@ -57,70 +56,74 @@ import React from 'react';
      },
    });
    return (
-     <form onSubmit={formik.handleSubmit}>
+       <Container>
+            <form onSubmit={formik.handleSubmit}>
 
-        <label htmlFor="email">Email Address</label>
-       <input
-         id="email"
-         name="email"
-         type="email"
-         onChange={formik.handleChange}
-         value={formik.values.email}
-       />
-       {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+            <label htmlFor="email">Email Address</label>
+            <input
+                id="email"
+                name="email"
+                type="email"
+                onChange={formik.handleChange}
+                value={formik.values.email}
+            />
+            {formik.errors.email ? <div>{formik.errors.email}</div> : null}
 
-       <label htmlFor="username">Username</label>
-       <input
-         id="username"
-         name="username"
-         type="text"
-         onChange={formik.handleChange}
-         value={formik.values.username}
-       />
-       {formik.errors.username ? <div>{formik.errors.username}</div> : null}
+            <label htmlFor="username">Username</label>
+            <input
+                id="username"
+                name="username"
+                type="text"
+                onChange={formik.handleChange}
+                value={formik.values.username}
+            />
+            {formik.errors.username ? <div>{formik.errors.username}</div> : null}
 
-       <label htmlFor="firstName">First Name</label>
-       <input
-         id="firstName"
-         name="firstName"
-         type="text"
-         onChange={formik.handleChange}
-         value={formik.values.firstName}
-       />
-       {formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
- 
-       <label htmlFor="lastName">Last Name</label>
-       <input
-         id="lastName"
-         name="lastName"
-         type="text"
-         onChange={formik.handleChange}
-         value={formik.values.lastName}
-       />
-       {formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
+            <label htmlFor="firstName">First Name</label>
+            <input
+                id="firstName"
+                name="firstName"
+                type="text"
+                onChange={formik.handleChange}
+                value={formik.values.firstName}
+            />
+            {formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
+        
+            <label htmlFor="lastName">Last Name</label>
+            <input
+                id="lastName"
+                name="lastName"
+                type="text"
+                onChange={formik.handleChange}
+                value={formik.values.lastName}
+            />
+            {formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
 
-       <label htmlFor="password">Password</label>
-       <input
-         id="password"
-         name="password"
-         type="password"
-         onChange={formik.handleChange}
-         value={formik.values.password}
-       />
-       {formik.errors.password ? <div>{formik.errors.password}</div> : null}
+            <label htmlFor="password">Password</label>
+            <input
+                id="password"
+                name="password"
+                type="password"
+                onChange={formik.handleChange}
+                value={formik.values.password}
+            />
+            {formik.errors.password ? <div>{formik.errors.password}</div> : null}
 
-       <label htmlFor="confirmPassword">Confirm Password</label>
-       <input
-         id="confirmPassword"
-         name="confirmPassword"
-         type="password"
-         onChange={formik.handleChange}
-         value={formik.values.confirmPassword}
-       />
-       {formik.errors.confirmPassword ? <div>{formik.errors.confirmPassword}</div> : null}
- 
-       <button type="submit">Submit</button>
-     </form>
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input
+                id="confirmPassword"
+                name="confirmPassword"
+                type="password"
+                onChange={formik.handleChange}
+                value={formik.values.confirmPassword}
+            />
+            {formik.errors.confirmPassword ? <div>{formik.errors.confirmPassword}</div> : null}
+        
+            <button type="submit">Register</button>
+            </form>
+
+            <p>Already have an account? Login <a href='/login'>here</a></p>
+     </Container>
    );
  };
 
