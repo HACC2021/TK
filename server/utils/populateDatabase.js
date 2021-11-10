315f7beaa1040313857762f4adcab466cf9f7f67
trey-sumida <tasumida@hawaii.edu>
2021-11-10 00:39:32 -1000

    Update populateDatabase.js

--- server/utils/populateDatabase.js
+++ server/utils/populateDatabase.js
@@ -4,9 +4,8 @@ import { nameToSlug } from './nameToSlug.js';
 import { parseTags } from './parseTags.js';
 
 export async function populateDatabase() {
-    const hasData = Trail.find();
-    if (hasData) {
-        console.log("herefadsfjasdl;fadsj;fdslfjdsafl;jasfds;fldsjf;sdfkdsfj;dlsfjsd");
+    const hasData = await Trail.find();
+    if (hasData.length === 0) {
         const data = JSON.parse(fs.readFileSync("/app/utils/trailData.geojson", "utf-8"));
         data.features.forEach( async (trail) => {
             const name = trail.properties.trailname;
