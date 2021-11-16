2a0f71efe3a9b00576f426d39e9e270481f707f9
Jackie Wong <wongzjackie@gmail.com>
2021-11-16 00:58:06 -1000

    Update login page.
    -Create css variables for the color theme.
    -Update colors and alignment of login form.
    -Add ux effects to form inputs.

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,83 +1,93 @@
 import React, { useContext } from 'react';
 import { useFormik } from 'formik';
-import Container from 'react-bootstrap/Container';
+// import Container from 'react-bootstrap/Container';
 import Axios from 'axios';
 import { useHistory } from 'react-router-dom';
 import AuthContext from '../store/auth-context';
 
- const validate = values => {
-   const errors = {};
+const validate = values => {
+  const errors = {};
 
-   if (!values.username) {
-       errors.username = 'Required';
-   }
+  if (!values.username) {
+    errors.username = 'Required';
+  }
 
-   if (!values.password) {
-       errors.password = 'Required';
-   }
- 
-   return errors;
- };
- 
- function Login() {
+  if (!values.password) {
+    errors.password = 'Required';
+  }
 
-    const authCtx = useContext(AuthContext);
-    const history = useHistory();
+  return errors;
+};
 
-   const formik = useFormik({
-     initialValues: {
-       username: '',
-       password: '',
-     },
-     validate,
-     onSubmit: values => {
-       Axios.post('http://localhost:5000/users/login', {username: values.username, password: values.password})
-       .then(response => {
-        authCtx.login(response.data.token);
-        history.replace('/profile');
-       })
-       .catch(error => {
-           console.log(error);
-       });
-     },
-   });
-   return (
-       <Container>
-            <form onSubmit={formik.handleSubmit}>
+function Login() {
+  const authCtx = useContext(AuthContext);
+  const history = useHistory();
 
-            <label htmlFor="username">Username</label>
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    validate,
+    onSubmit: values => {
+      Axios.post('http://localhost:5000/users/login', {
+        username: values.username,
+        password: values.password,
+      })
+        .then(response => {
+          authCtx.login(response.data.token);
+          history.replace('/profile');
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+  });
+  return (
+    <div className='login'>
+      <div className='login-container'>
+        <form onSubmit={formik.handleSubmit}>
+          <div className='input-container'>
+            <label htmlFor='username'>Username</label>
             <input
-                id="username"
-                name="username"
-                type="text"
-                onChange={formik.handleChange}
-                value={formik.values.username}
-                onBlur={formik.handleBlur}
+              id='username'
+              name='username'
+              type='text'
+              onChange={formik.handleChange}
+              value={formik.values.username}
+              onBlur={formik.handleBlur}
             />
-                   {formik.touched.username && formik.errors.username ? (
-         <div>{formik.errors.username}</div>
-       ) : null}
-
-            <label htmlFor="password">Password</label>
+            {formik.touched.username && formik.errors.username ? (
+              <div className='input-required'>{formik.errors.username}</div>
+            ) : null}
+          </div>
+          <div className='input-container'>
+            <label htmlFor='password'>Password</label>
             <input
-                id="password"
-                name="password"
-                type="password"
-                onChange={formik.handleChange}
-                value={formik.values.password}
-                onBlur={formik.handleBlur}
+              id='password'
+              name='password'
+              type='password'
+              onChange={formik.handleChange}
+              value={formik.values.password}
+              onBlur={formik.handleBlur}
             />
-                {formik.touched.password && formik.errors.password ? (
-                    <div>{formik.errors.password}</div>
-                ) : null}
-        
-            <button type="submit">Login</button>
-            </form>
+            {formik.touched.password && formik.errors.password ? (
+              <div className='input-required'>{formik.errors.password}</div>
+            ) : null}
+          </div>
+          <div className='input-container'>
+            <button type='submit' className='login-btn'>
+              Login
+            </button>
+          </div>
+        </form>
 
-            <p>Don't have an account? Register <a href='/register'>here</a></p>
-     </Container>
-   );
- };
+        <p>
+          Don't have an account? Register <a href='/register'>here</a>.
+        </p>
+      </div>
+    </div>
+  );
+}
 
- export default Login;
-
+export default Login;
